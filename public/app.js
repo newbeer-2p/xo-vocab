315f781a64eb2efa69b5914b1f1a42b04ccc3148
Newbeer <newbeerpongsakorn@gmail.com>
2022-03-29 12:34:44 +0700

    Setup Firebase

---
+++ public/app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSKAKXxbMpBJATfwp85PukyUWVufJ06HE",
+    authDomain: "xo-vocab.firebaseapp.com",
+    databaseURL: "https://xo-vocab-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "xo-vocab",
+    storageBucket: "xo-vocab.appspot.com",
+    messagingSenderId: "1037749967870",
+    appId: "1:1037749967870:web:45d790ea5953b79d523eaf",
+    measurementId: "G-QRQC73DHR0"
+};
+
+firebase.initializeApp(firebaseConfig);
