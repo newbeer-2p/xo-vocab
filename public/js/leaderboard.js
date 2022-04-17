55b221bf2a12d3eb8e45bc905b65154a2aa45c0f
Pathom <56293395+Pathompong123@users.noreply.github.com>
2022-04-17 11:24:13 +0700

    Updata leaderboard

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,18 +1,35 @@
-refUsers.orderByChild("win").limitToFirst(10).on("child_added", (snap) => {
-    console.log(snap.val());
-});
+//refUsers.orderByChild("win").limitToFirst(10).on("child_added", (snap) => {
+//    console.log(snap.val());
+//});
+refUsers.orderByChild("win").on("value", (data) => {
+    const newData = sortObjectReverse(data)
+})
 
+function sortObjectReverse(obj){
+    let toArray = [];
+    obj.forEach((o) => {
+        let newArray = []
+        newArray.push(o.val())
+        for (const ar of toArray){
 
-refUsers.orderByChild("win").limitToFirst(5).once('value', function(snapshot) {
+            newArray.push(ar)
+        }
+        toArray = newArray
+    })
+
+    return toArray
+}
+
+refUsers.orderByChild("win").limitToLast(10).once('value', function(snapshot) {
     var tr;
-    var rank = 1;
+    var rank = 10;
 
     snapshot.forEach((snap) => {
         tr = $('<ul/>');
-        tr.append("<li><p>" + rank + "</p></li>");
-        tr.append("<li><p>" + snap.val().email.toLocaleString() + "</p></li>");
-        tr.append("<li><p>" + snap.val().win.toLocaleString() + "</p></li>");
+        tr.append("<li class='list-rank'><p>" + rank + "</p></li>");
+        tr.append("<li class='list-player'><p>" + snap.val().name.toLocaleString() + "</p></li>");
+        tr.append("<li class='list-win'><p>" + snap.val().win.toLocaleString() + "</p></li>");
         $('table').append(tr);
-        rank = rank + 1;
+        rank = rank - 1;
 });
 })
